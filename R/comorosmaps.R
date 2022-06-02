eb233fef3689c2f9861875cdc56b94ae07502e50
Housni Hassani <happrend2@gmail.com>
2022-06-02 13:28:06 +0200

    submit

--- R/comorosmaps.R
+++ R/comorosmaps.R
@@ -2,11 +2,14 @@
 #'
 #' Draw  maps of Comoros Islands.
 #'
-#'
 #' `comoros()`uses the sf package to plot by plotting the geometry rather than every column and leave the plot pref ready for overplotting with other data
 #'
 #' @param x     Name of the data set to use. The default is `comoros()`, It draws Comoro Islands as one object without commune.
 #' @param pref  Choose to map with prefecture area ("pref" = TRUE) or without prefecture area ("pref" = FALSE)
+#' @return The data set used is in `sf` format
+#' @export
+#' @importFrom sf st_geometry
+#' @importFrom graphics plot
 #' @examples
 #' ## Map Comoro Islands as one object without prefecture area
 #' comoros()
@@ -17,30 +20,27 @@
 #' ## Map Comoros Islands as 3 object (Grande Comore, Anjouan, Mohéli) with prefecture area
 #' comoros(x="island",pref=TRUE)
 #'
-#' @return The data set used is in `sf` format
-#' @export
-#'
 comoros <- function(x="country", pref=FALSE) {
   if (x=="country" & pref==TRUE) {
-    x <- comoromaps_data %>%
+     km <- comoromaps_data %>%
       filter (adminCode %in% c("KM11","KM12","KM13","KM14","KM15","KM21","KM22","KM23","KM24","KM25","KM26","KM27","KM28","KM29","KM31","KM32","KM33"))%>%
       select(name,geometry)
   } else if (x=="country" & pref==FALSE) {
-    x <- comoromaps_data %>%
+    km <- comoromaps_data %>%
       filter (adminCode %in% c("KM"))%>%
       select(name,geometry)
   } else if (x=="island" & pref==FALSE) {
-    x <- comoromaps_data %>%
+    km <- comoromaps_data %>%
       filter (adminCode %in% c("KM1","KM2","KM3"))%>%
       select(name,geometry)
   } else if (x=="island" & pref==TRUE) {
-    x <- comoromaps_data %>%
+    km <- comoromaps_data %>%
       filter (adminCode %in% c("KM11","KM12","KM13","KM14","KM15","KM21","KM22","KM23","KM24","KM25","KM26","KM27","KM28","KM29","KM31","KM32","KM33"))%>%
       select(name,geometry)
   }else
-    x <- print("error")
-  plot(sf::st_geometry(x))
-  invisible(unique(x))
+    km <- print("error")
+  plot(sf::st_geometry(km))
+  invisible(unique(km))
 }
 #' Grande Comore
 #'
@@ -50,9 +50,10 @@ comoros <- function(x="country", pref=FALSE) {
 #' @param x     Name of the data set to use. The default is `grandeComore()`, It draws Grande Comore Island as one object without prefecture.
 #' @param pref  Choose to map with prefecture area ("pref" = TRUE) or without prefecture area ("pref" = FALSE)
 #'
-#'
 #' @return The data set used is in `sf` format
 #' @export
+#' @importFrom sf st_geometry
+#' @importFrom graphics plot
 #' @examples
 #' ## Map Grande Comore Island
 #' grandeComore ()
@@ -61,18 +62,47 @@ comoros <- function(x="country", pref=FALSE) {
 #'
 grandeComore <- function(x="grande comore", pref=FALSE) {
   if (x=="grande comore" & pref ==TRUE) {
-    x <- comoromaps_data %>%
+    km <- comoromaps_data %>%
       filter (adminCode %in% c("KM21","KM22","KM23","KM24","KM25","KM26","KM27",
                                "KM28","KM29"))%>%
       select(name,geometry)
   } else if (x=="grande comore" & pref == FALSE) {
-    x <- x <- comoromaps_data %>%
+    km <- comoromaps_data %>%
       filter (adminCode %in% c("KM2"))%>%
       select(name,geometry)
   } else
-    x <- print("error")
-  plot(sf::st_geometry(x))
-  invisible(unique(x))
+    km <- print("error")
+  plot(sf::st_geometry(km))
+  invisible(unique(km))
+}
+#' Moheli
+#'
+#' Draw a map for Moheli Islands
+#'
+#' @param x     Name of the data set to use. The default is `moheli()`, It draws Moheli Islands as one object without prefecture.
+#' @param pref  Choose to map with prefecture area ("pref" = TRUE) or without prefecture area ("pref" = FALSE)
+#' @return The data set used is in `sf` format
+#' @export
+#' @importFrom sf st_geometry
+#' @importFrom graphics plot
+#' @examples
+#' ## Map Moheli Island
+#' moheli ()
+#' ## Map Moheli Island with prefecture area
+#' moheli (pref = TRUE)
+moheli <- function(x="moheli", pref=FALSE) {
+  if (x=="moheli" & pref ==TRUE) {
+    km <- comoromaps_data %>%
+      filter (adminCode %in% c("KM31","KM32","KM33"))%>%
+      select(name,geometry)
+  } else if (x=="moheli" & pref == FALSE) {
+    km <- comoromaps_data %>%
+      filter (adminCode %in% c("KM3")) %>%
+      select(name,geometry)
+  } else
+    km <- print("error")
+  plot(sf::st_geometry(km))
+  invisible(unique(km))
 }
 #'
 #' Anjouan
@@ -81,58 +111,28 @@ grandeComore <- function(x="grande comore", pref=FALSE) {
 #'
 #' @param x     Name of the data set to use. The default is `anjouan()`, It draws Anjouan Island as one object without prefecture.
 #' @param pref  Choose to map with prefecture area ("pref" = TRUE) or without prefecture area ("pref" = FALSE)
+#' @return The data set used is in `sf` format
+#' @export
+#' @importFrom sf st_geometry
+#' @importFrom graphics plot
 #' @examples
 #' ## Map Anjouan Island.
 #' anjouan ()
 #' ## Map Anjouan Island with prefecture area.
 #' anjouan (pref = TRUE)
 #'
-#'
-#' @return The data set used is in `sf` format
-#' @export
-#'
 anjouan <- function(x="anjouan", pref=FALSE) {
   if (x=="anjouan" & pref ==TRUE) {
-    x <- comoromaps_data %>%
+    km <- comoromaps_data %>%
       filter (adminCode %in% c("KM11","KM12","KM13","KM14","KM15"))%>%
       select(name,geometry)
   } else if (x=="anjouan" & pref == FALSE) {
-    x <- x <- comoromaps_data %>%
+    km <- comoromaps_data %>%
       filter (adminCode %in% c("KM1"))%>%
       select(name,geometry)
   } else
-    x <- print("error")
-  plot(sf::st_geometry(x))
-  invisible(unique(x))
-}
-#'
-#' Moheli
-#'
-#' Draw a map for Moheli Islands
-#'
-#' @param x     Name of the data set to use. The default is `moheli()`, It draws Moheli Islands as one object without prefecture.
-#' @param pref  Choose to map with prefecture area ("pref" = TRUE) or without prefecture area ("pref" = FALSE)
-#' @examples
-#' ## Map Moheli Island
-#' moheli ()
-#' ## Map Moheli Island with prefecture area
-#' moheli (pref = TRUE)
-#'
-#' @return The data set used is in `sf` format
-#' @export
-#'
-moheli <- function(x="moheli", pref=FALSE) {
-  if (x=="moheli" & pref ==TRUE) {
-    x <- comoromaps_data %>%
-      filter (adminCode %in% c("KM31","KM32","KM33"))%>%
-      select(name,geometry)
-  } else if (x=="moheli" & pref == FALSE) {
-    x <- x <- comoromaps_data %>%
-      filter (adminCode %in% c("KM3")) %>%
-      select(name,geometry)
-  } else
-    x <- print("error")
-  plot(sf::st_geometry(x))
-  invisible(unique(x))
+    km <- print("error")
+  plot(sf::st_geometry(km))
+  invisible(unique(km))
 }
 
